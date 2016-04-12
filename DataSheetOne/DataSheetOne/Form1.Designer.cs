0888342b49376737d6295193bd318f189dca7eae
Michael Miles <boat081062@gmail.com>
2016-04-12 05:09:13 -0500

    Form creation in process

--- DataSheetOne/DataSheetOne/Form1.Designer.cs
+++ DataSheetOne/DataSheetOne/Form1.Designer.cs
@@ -28,40 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tab_Control = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
+            this.tab_Control.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // tab_Control
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(54, 52);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(867, 466);
-            this.tabControl1.TabIndex = 0;
+            this.tab_Control.Controls.Add(this.tabPage1);
+            this.tab_Control.Controls.Add(this.tabPage2);
+            this.tab_Control.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tab_Control.Location = new System.Drawing.Point(54, 52);
+            this.tab_Control.Name = "tab_Control";
+            this.tab_Control.SelectedIndex = 0;
+            this.tab_Control.Size = new System.Drawing.Size(867, 466);
+            this.tab_Control.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Location = new System.Drawing.Point(4, 34);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(859, 433);
+            this.tabPage1.Size = new System.Drawing.Size(859, 428);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.Text = "Employee Data";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Location = new System.Drawing.Point(4, 34);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(859, 433);
+            this.tabPage2.Size = new System.Drawing.Size(859, 428);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "Misc Data";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
@@ -69,17 +70,17 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(978, 600);
-            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.tab_Control);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.tabControl1.ResumeLayout(false);
+            this.tab_Control.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl tab_Control;
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
     }
